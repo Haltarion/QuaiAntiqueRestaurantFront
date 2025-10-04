36c1f77b5e3ec9541b44ab63fd7cc0ba680f6781
Haltarion <michael.bourloux@gmail.com>
2025-10-04 11:05:42 +0200

    Connexion de l'API back au front

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -13,6 +13,8 @@ inputMail.addEventListener("keyup", validateForm);
 inputPassword.addEventListener("keyup", validateForm);
 inputValidationPassword.addEventListener("keyup", validateForm);
 
+btnValidation.addEventListener("click", InscrireUtilisateur);
+
 //Function permettant de valider tout le formulaire
 function validateForm() {
   const nomOk = validateRequired(inputNom);
@@ -84,4 +86,28 @@ function validateRequired(input) {
     input.classList.add("is-invalid");
     return false;
   }
+}
+
+function InscrireUtilisateur() {
+  let myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  let raw = JSON.stringify({
+    firstName: "Test fetch",
+    lastName: "test fetch",
+    email: "testdepuisQuaiAntique@email.com",
+    password: "Azerty11",
+  });
+
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("https://127.0.0.1:8000/api/registration", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
 }
